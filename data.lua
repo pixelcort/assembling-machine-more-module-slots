d81b2d07311884caba3571ce40d5134c8c2d7fcb
Cortland Klein <me@pixelcort.com>
2022-01-29 23:08:54 +0900

    Initial commit

---
+++ data.lua
@@ -0,0 +1,17 @@
+
+-- data.raw["assembling-machine"]["5d-assembling-machine-04"].module_specification.module_slots = 6
+-- data.raw["assembling-machine"]["5d-assembling-machine-05"].module_specification.module_slots = 8
+-- data.raw["assembling-machine"]["5d-assembling-machine-06"].module_specification.module_slots = 10
+-- data.raw["assembling-machine"]["5d-assembling-machine-07"].module_specification.module_slots = 12
+-- data.raw["assembling-machine"]["5d-assembling-machine-08"].module_specification.module_slots = 14
+-- data.raw["assembling-machine"]["5d-assembling-machine-09"].module_specification.module_slots = 16
+-- data.raw["assembling-machine"]["5d-assembling-machine-10"].module_specification.module_slots = 18
+
+
+data.raw["assembling-machine"]["5d-assembling-machine-04"].module_specification.module_slots = 8
+data.raw["assembling-machine"]["5d-assembling-machine-05"].module_specification.module_slots = 16
+data.raw["assembling-machine"]["5d-assembling-machine-06"].module_specification.module_slots = 32
+data.raw["assembling-machine"]["5d-assembling-machine-07"].module_specification.module_slots = 64
+data.raw["assembling-machine"]["5d-assembling-machine-08"].module_specification.module_slots = 128
+data.raw["assembling-machine"]["5d-assembling-machine-09"].module_specification.module_slots = 256
+data.raw["assembling-machine"]["5d-assembling-machine-10"].module_specification.module_slots = 512
